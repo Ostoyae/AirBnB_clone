af8a64450f17e3878340e0c33c1c5119259e2b66
Martin Smith <msmith3dart@icloud.com>
2019-02-15 03:25:01 +0000

    Add Functionality
    Added:
      - create
      - show
      - all

--- console.py
+++ console.py
@@ -4,7 +4,7 @@ Modules for HBNB console for the AirBnB clone project
 """
 import cmd
 from models.base_model import BaseModel
-from models import storage
+from models.city import City
 
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
@@ -63,13 +63,83 @@ class HBNBCommand(cmd.Cmd):
                 cls = globals()[line]
                 obj = cls()
                 # TODO: conver to json add to file and read ID
-                storage.new(obj)
-                storage.save()
+#                obj.save()
+                print(obj.id)
             except:
                 print("** class doesn't exist **")
         else:
             print("** class name missing **")
 
+    def do_show(self, line):
+        """
+        :params line: Name of Class and id
+
+        :usage: 
+            $ show <class name>.id
+            $ [class] (id) {<dict of class>}
+
+        :return: None
+        """
+        if not line:
+            print("** class name missing **")
+        else:
+            args = line.split(' ')
+            try:
+                cls = globals()[args[0]].__name__
+                if args[1]:
+                    ident = args[1]
+            except KeyError:
+                print("** class doesn't exist **")
+            except IndexError:
+                print("** instance id missing **")
+            
+            try:
+                #temp until FileStorage is implmented
+                a = {"BaseModel.42": {"id": "42"}}
+                a["{}.{}".format(cls,ident)]
+#                storage.all()[cls+'.'+ident]
+            except KeyError:
+                print("** no instance found **")
+    
+    def do_all(self, line=None):
+        """
+        :params line: Name of Class
+
+        :usage: 
+        
+        :example 1:
+            $ all
+            $ [[BaseModel] (id) {<dict of class>}, [City] (id) {<dict of
+            class>},..]
+        
+        :example 2:
+            $ all BaseModel
+            $ [[BaseModel] (id) {<dict of class>}, [BaseModel] (id) {<dict of
+            class>}, ...]
+
+        :return: None
+        """
+        
+        ls_d = list()
+        storage = {"BaseModel.42": {"id": "42"}, "BaseModel.1337": {"id": "1337"},
+                "City.101": {"id": "101"}}
+        if line:
+            for key, v in storage.items():
+                if key.startswith(line):
+                    obj = globals()[line](**v)
+                    ls_d.append(str(obj))
+                    del obj
+
+        else:
+            for key, v in storage.items():
+                arg = key.split('.')
+                obj = globals()[arg[0]](**v)
+                ls_d.append(str(obj))
+                del obj
+        print(ls_d)
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
+
+
+
