cb5e6887d21dcf7838b84d67e7de3eebbdbb5344
ConnorBrereton <breretonconnor@gmail.com>
2019-02-17 07:52:58 +0000

    Created link from BaseModel to FileStorage

--- models/base_model.py
+++ models/base_model.py
@@ -38,6 +38,8 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            models.storage.new(self)
+            models.storage.save()
 
     def __str__(self):
         """Returns string repr. of `BaseClass` model."""
@@ -49,6 +51,8 @@ class BaseModel():
         """Updates `updated_at` attribute."""
 
         self.updated_at = datetime.now()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """Converts time attributes to ISO 8601 format."""
