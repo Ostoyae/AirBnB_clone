007e649a854230c0647ce9fa8943b988d620f9ba
ConnorBrereton <breretonconnor@gmail.com>
2019-02-19 03:24:15 +0000

    PEP8 fix

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,17 +1,34 @@
 #!/usr/bin/python3
-"""Test module for State class
+"""
+Test module for State class
 """
 
 import unittest
 from models.state import State
 
+
 class TestState(unittest.TestCase):
-    
+    """
+    Unittest for State instance.
+    """
+
     def setUp(self):
+        """
+        Create an instance.
+        """
+
         self.state = State()
 
     def tearDown(self):
+        """
+        Clean up an instance.
+        """
+
         del self.state
 
     def test_init(self):
+        """
+        Test instance created.
+        """
+
         self.assertTrue(isinstance(self.state, State))
