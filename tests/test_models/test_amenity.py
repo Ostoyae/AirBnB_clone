6f147d7a5bcd349188b3391151581d69d04641be
ConnorBrereton <breretonconnor@gmail.com>
2019-02-19 03:08:26 +0000

    PEP8 fix

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,17 +1,35 @@
 #!/usr/bin/python3
-"""Test module for Amenity class
+"""
+Test module for Amenity class.
 """
 
 import unittest
 from models.amenity import Amenity
 
+
 class TestAmenity(unittest.TestCase):
-    
+    """
+    Unittest for
+    Amenity class.
+    """
+
     def setUp(self):
+        """
+        Creates an instance.
+        """
+
         self.amenity = Amenity()
 
     def tearDown(self):
+        """
+        Cleans up an instance.
+        """
+
         del self.amenity
 
     def test_init(self):
+        """
+        Test instance.
+        """
+
         self.assertTrue(isinstance(self.amenity, Amenity))
