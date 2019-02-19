10f33fb5e86380daf6e73011e25b8aa9ecbf473c
ConnorBrereton <breretonconnor@gmail.com>
2019-02-19 19:59:19 +0000

    pep8, teardown, attributes, docstring tests added

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -16,6 +16,36 @@ class TestUser(unittest.TestCase):
     def setUp(self):
         self.objects = storage.all()
 
+    def tearDown(self):
+        try:
+            os.remove("file.json")
+
+        except FileNotFoundError:
+            pass
+
+    def test_style_check(self):
+        pep8style = pep8.StyleGuide(quiet=True)
+        p = pep8style.check_files(['models/user.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
+
+    def test_attr(self):
+        self.assertTrue('id' in self.user.__dict__)
+        self.assertTrue('created_at' in self.user.__dict__)
+        self.assertTrue('updated_at' in self.user.__dict__)
+        self.assertTrue('name' in self.user.__dict__)
+
+    def test_documentation(self):
+        self.assertIsNotNone(User.__doc__)
+
+    def test_class(self):
+        self.assertTrue(isinstance(self.user, User))
+
+    def test_id(self):
+        self.assertTrue(type(self.user.id), int)
+
+    def test_name(self):
+        self.assertTrue(type(self.user.name), str)
+
     def test_class(self):
         self.assertTrue(isinstance(self.user, User))
 
