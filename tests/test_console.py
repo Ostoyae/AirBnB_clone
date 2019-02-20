ad83af9d6992a7e416460387e2fb7229079f7245
ConnorBrereton <breretonconnor@gmail.com>
2019-02-20 17:38:46 +0000

    Test console backbone added'

---
+++ tests/test_console.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Unittest for
+the console.
+"""
+
+
+import os
+import pep8
+import unittest
+
+class TestConsole(unittest.TestCase):
+    """
+    Unittest for
+    the console.
+    """ 
