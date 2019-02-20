3fa04850ab2402e3088996710be048ee9049fb60
Martin Smith <msmith3dart@icloud.com>
2019-02-20 23:26:54 +0000

    upate state test

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -15,8 +15,8 @@ class TestState(unittest.TestCase):
     Unittest for State instance.
     """
 
-    user = State()
-    o_id = '{}.{}'.format('State', user.id)
+    state = State()
+    o_id = '{}.{}'.format('State', state.id)
 
     def setUp(self):
         self.objects = storage.all()
@@ -26,47 +26,50 @@ class TestState(unittest.TestCase):
 
     def test_style_check(self):
         pep8style = pep8.StyleGuide(quiet=True)
-        p = pep8style.check_files(['models/user.py'])
+        p = pep8style.check_files(['models/state.py'])
         self.assertEqual(p.total_errors, 0, "fix pep8")
 
     def test_attr(self):
-        self.assertTrue('id' in self.user.__dict__)
-        self.assertTrue('created_at' in self.user.__dict__)
-        self.assertTrue('updated_at' in self.user.__dict__)
+        self.assertTrue('id' in self.state.__dict__)
+        self.assertTrue('created_at' in self.state.__dict__)
+        self.assertTrue('updated_at' in self.state.__dict__)
 
     def test_documentation(self):
         self.assertIsNotNone(State.__doc__)
 
     def test_class(self):
-        self.assertTrue(isinstance(self.user, State))
+        self.assertTrue(isinstance(self.state, State))
 
     def test_id(self):
-        self.assertTrue(type(self.user.id), int)
+        self.assertTrue(type(self.state.id), int)
 
     def test_name(self):
-        self.assertTrue(type(self.user.name), str)
+        self.assertTrue(type(self.state.name), str)
 
     def test_class(self):
-        self.assertTrue(isinstance(self.user, State))
+        self.assertTrue(isinstance(self.state, State))
 
     def test_field_name(self):
-        d = self.objects[self.o_id]
+        d = self.objects[self.o_id].to_dict()
         self.assertTrue(any(k == 'name' for k in d.keys()))
 
     def test_update(self):
-        cur_time = self.objects[self.o_id]['updated_at']
-        self.user.save()
-        new_time = self.user.to_dict()['updated_at']
+        cur_time = self.state.updated_at
+        self.state.save()
+        new_time = self.state.updated_at
         self.assertNotEqual(cur_time, new_time)
 
     def test_load_from_dict(self):
-        new = State(**self.objects[self.o_id])
-        self.assertEqual(new.to_dict(), self.objects[self.o_id])
+        new = State(**self.objects[self.o_id].to_dict())
+        self.assertEqual(new.to_dict(), self.objects[self.o_id].to_dict())
 
     def test_no_private_attrs(self):
-        for k in self.objects[self.o_id].keys():
+        for k in self.objects[self.o_id].to_dict().keys():
             if k is not '__class__':
                 self.assertTrue(('__' not in k))
 
     def test_class_attr(self):
-        self.assertEqual(self.objects[self.o_id]['__class__'], 'State')
+        self.assertEqual(
+                self.objects[self.o_id].to_dict()['__class__'],
+                'State'
+                )
